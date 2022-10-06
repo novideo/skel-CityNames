fbefaaa61a013128ccb2dd9c827dcfc413503d85
novideo <97853114+novideo@users.noreply.github.com>
2022-10-05 23:53:20 -0700

    Update CityNetwork.java

--- src/main/java/citynetwork/CityNetwork.java
+++ src/main/java/citynetwork/CityNetwork.java
@@ -7,20 +7,37 @@ public class CityNetwork {
     public final List<CityTLA> _cities = new ArrayList<>();
 
     public boolean checkRep() {
-        for (int i = 0; i < _cities.size(); i++) {
-            CityTLA city1 = _cities.get(i);
-            if (city1.checkRep() == false) {
-                return false;
+        List<String> cityNameList = new ArrayList<>();
+
+        //Add each element in to the respected list
+        for(int i = 0; i < _cities.size(); i++) {
+            cityNameList.add(_cities.get(i)._cityName);
+        }
+
+        for(int i = 0; i < _cities.size(); i++) { //Go through each code
+            String currentCode = _cities.get(i)._cityCode; //Get current city name for checking
+            int count = 0;
+            for(int j = 0; j < _cities.size(); j++) { //Scan through every city with each code
+                String currentCity = cityNameList.get(j);
+                if(Utils.isValidCode(currentCity, currentCode)) count++;
             }
-            for (int j = i+1; j < _cities.size(); j++) {
-                CityTLA city2 = _cities.get(j);
-                if (Utils.isValidCode(city2._cityName, city1._cityCode)) {
-                    return false;
-                }
-            }
+            if(count != 1) return false;
         }
         return true;
     }
+
+//        for (int i = 0; i < _cities.size(); i++) {
+//            CityTLA city1 = _cities.get(i);
+//            if (city1.checkRep() == false) {
+//                return false;
+//            }
+//            for (int j = i+1; j < _cities.size(); j++) {
+//                CityTLA city2 = _cities.get(j);
+//                if (Utils.isValidCode(city2._cityName, city1._cityCode)) {
+//                    return false;
+//                }
+//            }
+//        }
 
     /**
      *
